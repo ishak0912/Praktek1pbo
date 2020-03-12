331f02cd8b222d962ffc85bd692a4179cf42c5c8
ishak0912 <62091866+ishak0912@users.noreply.github.com>
2020-03-12 15:11:52 +0800

    Add files via upload

---
+++ ISHAK.java
@@ -0,0 +1,10 @@
+ import java.io.*;
+class KONTOL{
+	public static void main (string[] arg)
+	{
+		System.out.println("hallo nama saya andi ishak");
+		System.out.println("nim saya 32019003");
+		System.out.println("jurusan saya D3 TEKNIK INFORMATIKA");
+		
+	}
+} 
